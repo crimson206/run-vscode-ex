4951bf843fb90d69d3e44a8c37d711d7ea591012
Sisung Kim <sisung.kim1@gmail.com>
2024-08-17 13:23:47 +0200

    Updates

---
+++ assets/README-D1wwlBNB.js
@@ -0,0 +1,13 @@
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as i}from"./index-CcnH5Kt0.js";import"./index-RYns6xqu.js";function s(o){const e={a:"a",code:"code",h1:"h1",li:"li",p:"p",ul:"ul",...i(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h1,{id:"run-code-extension",children:"Run Code Extension"}),`
+`,n.jsx(e.p,{children:"This extension is basically to run vscode extensions programmatically."}),`
+`,n.jsxs(e.p,{children:["For more details, visit ",n.jsx(e.code,{children:"Homepaage"}),". For the quick understanding of this extension, read ",n.jsx(e.code,{children:"Core"})," or ",n.jsx(e.code,{children:"Quick Start"})," page first."]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[`
+`,n.jsxs(e.p,{children:[n.jsx(e.a,{href:"https://crimson206.github.io/run-vscode-ex/",rel:"nofollow",children:"Homepage"}),`
+: Blog Page including Docs`]}),`
+`]}),`
+`,n.jsxs(e.li,{children:[`
+`,n.jsxs(e.p,{children:[n.jsx(e.a,{href:"https://github.com/crimson206/run-vscode-ex",rel:"nofollow",children:"Git"}),`
+: Git Repository`]}),`
+`]}),`
+`]})]})}function l(o={}){const{wrapper:e}={...i(),...o.components};return e?n.jsx(e,{...o,children:n.jsx(s,{...o})}):s(o)}export{l as default};
